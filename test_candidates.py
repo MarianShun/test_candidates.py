11ead93a1be430fddffbba1834d0d8dc15ea41b9
MarianShun <37655860+MarianShun@users.noreply.github.com>
2018-05-09 15:48:55 +0300

    Update test_candidates.py

--- test_candidates.py
+++ test_candidates.py
@@ -1,8 +1,8 @@
 import pytest
 from candidates import Candidate
-maxid = 2100
+maxid = 1163
 
-@pytest.mark.parametrize('cid', range(2064,maxid))
+@pytest.mark.parametrize('cid', range(1129,maxid))
 def test_candidate_status_code_get(cid):
     assert Candidate().get(cid).status_code == 200
 
@@ -10,7 +10,6 @@ class TestsPostDel:
     names = ['QA Trainee', 'QA Intern', 'Junior QA', 'QA Engineer', 'Junior Automation QA', 'Delete']  
     @pytest.mark.parametrize(('position'), names)	
     def test_candidate_status_code_post(self, position):
-        assert Candidate().post('Marian Shun', position).status_code == 201
-		
+        assert Candidate().post('Marian Shun', position).status_code == 201	
     def test_candidate_status_code_del(self):
         assert Candidate().delete(maxid+6).status_code == 200
